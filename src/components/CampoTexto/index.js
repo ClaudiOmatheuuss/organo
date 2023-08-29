1c1e844adce1edaadc3fe13cb3ee562baf3eb602
Claud10matheus <claudiom.pst@gmail.com>
2023-08-29 17:44:25 -0300

    trabalhando com ouvinte de evento e props

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -5,9 +5,9 @@ const CampoTexto = (props) => { //recebe arrow function com propriedades como pa
         //JSX
         <div className="campo-texto">
             <label>{props.label}</label> 
-            <input placeholder={props.placeholder}/>
+            <input required={props.obrigatorio} placeholder={props.placeholder}/> 
         </div>
-    )
+    ) //props.obrigatorio for true então o input será required
 }// retorna label e placeholder do input de acordo com a string que atribuimos a essa propriedades do componente no arquivo da nossa aplicação react
 
 export default CampoTexto
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -13,14 +13,19 @@ const Formulario = () => {
         'Mobile',
         'Inovação e gestão'
     ]
+    const aoEnviar = (evento) => {
+        evento.preventDefault()
+        console.log("O form foi enviado!")
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoEnviar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-                <ListaSuspensa label="Time" itens={times}/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/> 
                 <Botao texto="Criar card" />
             </form>
         </section>
