da93e5d80c00e289b3b60719cc87787fc194e5b6
Claud10matheus <claudiom.pst@gmail.com>
2023-08-29 09:31:01 -0300

    deletando diretorios padrão do reactApp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './components/Banner/banner.js';
 
 
@@ -7,20 +5,6 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite src/App.js e salve para reiniciar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
