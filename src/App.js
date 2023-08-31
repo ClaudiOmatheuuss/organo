15c218965e3a87189b5f78a5915c785b9c34b9f6
Claud10matheus <claudiom.pst@gmail.com>
2023-08-31 12:38:10 -0300

    criando componente Time de forma dinâmica

--- src/App.js
+++ src/App.js
@@ -4,13 +4,49 @@ import Time from './components/Time';
 
 
 function App() {
+  const Times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9' 
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF' 
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: 'F0F8E2' 
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8' 
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5' 
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9' 
+    },
+    {
+      nome: 'Inovação e gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF' 
+    }
+  ]
+
   return (
     <div className="App">
       <Banner />
       <Formulario />
-      <Time nome="Programação"/>
-      <Time nome="Front-end"/>
-      <Time nome="DevOps"/>
+      {Times.map(time => <Time nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
     </div>
   );
 }
