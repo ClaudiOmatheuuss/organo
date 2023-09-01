4ab729ab935ef02de05361adc63d55b40cf091e9
Claud10matheus <claudiom.pst@gmail.com>
2023-09-01 10:37:54 -0300

    controlando o estado dos inputs

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => { //recebe arrow function com propriedades como parametro
+   
+    const placeholderModificada = `${props.placeholder}...`
+
+    const [valor, setValor] = useState('') 
+
+    const aoDigitado = (evento) => {
+        setValor(evento.target.value)
+        console.log(valor)
+    }
+    
     return (
         //JSX
         <div className="campo-texto">
             <label>{props.label}</label> 
-            <input required={props.obrigatorio} placeholder={props.placeholder}/> 
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/> 
         </div>
     ) //props.obrigatorio for true então o input será required
 }// retorna label e placeholder do input de acordo com a string que atribuimos a essa propriedades do componente no arquivo da nossa aplicação react
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,14 +4,14 @@ import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = (props) => {
-    const aoEnviar = (evento) => {
+    const aoSalvar = (evento) => {
         evento.preventDefault()
         console.log("O form foi enviado!")
     }
 
     return (
         <section className='formulario'>
-            <form onSubmit={aoEnviar}>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
                 <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
