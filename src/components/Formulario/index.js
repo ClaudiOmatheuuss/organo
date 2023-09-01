9d2a5a5e0e057d2a4ec8bd0a89ba78bb097ad57a
Claud10matheus <claudiom.pst@gmail.com>
2023-09-01 15:40:59 -0300

    escondendo times vazios

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -19,6 +19,10 @@ const Formulario = (props) => {
             imagem: imagem,
             time: time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
 
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -4,13 +4,13 @@ import './Time.css'
 const Time = (props) => {
     const corDeFundo = props.corSecundaria
     return(
-        <section className='time' style={{backgroundColor: corDeFundo}}>
+        (props.colaboradores.length) > 0 && <section className='time' style={{backgroundColor: corDeFundo}}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
             {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} /> )}
             </div>
         </section>
     )
-}
+}// o time com colaboradores é criado apenas se o tamanho da lista de colaboradores do time for maior que 0
 
 export default Time
