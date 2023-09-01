235bc57964021cf0cec7ea607e0aaf21acfacb30
Claud10matheus <claudiom.pst@gmail.com>
2023-09-01 16:04:24 -0300

    resolvendo erro de key

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -7,7 +7,7 @@ const Time = (props) => {
         (props.colaboradores.length) > 0 && <section className='time' style={{backgroundColor: corDeFundo}}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
-            {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} /> )}
+            {props.colaboradores.map( colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} /> )}
             </div>
         </section>
     )
